c6ed4b72b76d5d424f436d4d5cf2672fd197ad27
PVCR <pvchennareddy@gmail.com>
2016-03-09 19:00:04 -0500

    WPF Change View

---
+++ ChangeView/Content/Page1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeView.Content
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : UserControl
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UniformGrid ct = UIHelper.FindChild<UniformGrid>(Application.Current.MainWindow, "bodyContainer");
+
+            if (ct != null)
+            {
+                ct.Children.Clear();
+                ct.Children.Add(GetPage2());
+            }
+
+        }
+
+        private UIElement GetPage2()
+        {
+            return new Page2() { Margin = new Thickness(5) };
+        }
+    }
+}
---
+++ ChangeView/Content/Page2.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeView.Content
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : UserControl
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UniformGrid ct =   UIHelper.FindChild<UniformGrid>(Application.Current.MainWindow, "bodyContainer");
+
+            if(ct != null)
+            {
+                ct.Children.Clear();
+                ct.Children.Add(GetPage1());
+            }
+
+        }
+
+        private UIElement GetPage1()
+        {
+            return new Page1() { Margin=new Thickness(5) };
+        }
+    }
+}
